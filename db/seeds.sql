d4368f6f3a82a548fe3c2a80718cd034fa2d50e8
ranghel <anghelramona30@gmail.com>
2017-05-14 10:46:17 -0400

    cleaned up the files

--- db/seeds.sql
+++
@@ -1,9 +0,0 @@
-
-INSERT INTO burgers (burger_name)
-VALUES("Big Mac");
-
-INSERT INTO burgers (burger_name)
-VALUES("Bacon Cheese Burger");
-
-INSERT INTO burgers (burger_name)
-VALUES("World's Most Famos Burger");
